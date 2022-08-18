0915f66058b58adf05a20302cbca561b87490f78
bent_shrimp <park-jinwoo03@naver.com>
2022-08-18 12:55:11 +0900

    [Feat] recording and upload to google drive

--- detect.py
+++ detect.py
@@ -24,12 +24,17 @@ Usage - formats:
                                          yolov5s_edgetpu.tflite     # TensorFlow Edge TPU
 """
 
-# from picamera import PiCamera
-# from googleapiclient.discovery import build
-# from httplib2 import Http
-# from oauth2client import file, client, tools
+from picamera import PiCamera
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
 
-import argparse
+try :
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
 import os
 import platform
 import sys
@@ -39,7 +44,7 @@ import torch
 import torch.backends.cudnn as cudnn
 
 # camera setting
-# camera = PiCamera()
+camera = PiCamera()
 # camera.rotation = 180
 
 FILE = Path(__file__).resolve()
@@ -180,15 +185,49 @@ def run(
 
             s += f"({pre_n}, {n}, {cnt}, {cam_on})\n"  # add to string
 
-            if cnt > 30:
+            if cnt > 300:
                 # start recording
                 if not cam_on and n != 0:
+                    camera.start_preview()
+                    camera.start_recording('/home/pi/Desktop/video.h264')
                     cam_on = True
-                    s += "package detected! "
-                # stop recoring
-                elif cam_on and n == 0:
+                    s += "package detected! "   # add to string
+
+                # stop recording
+                if cam_on and n == 0:
+                    camera.stop_recording()
+                    camera.stop_preview()
+
+                    if os.path.isfile("video.mp4"):
+                        os.remove("video.mp4")
+                    os.system("sh codec.sh")
+
+                    SCOPES = 'https://www.googleapis.com/auth/drive.file'
+                    store = file.Storage('storage.json')
+                    creds = store.get()
+
+                    if not creds or creds.invalid:
+                        print("make new storage data file ")
+                        flow = client.flow_from_clientsecrets('credentials.json', SCOPES) # 인증정보 파일
+                        creds = tools.run_flow(flow, store, flags) if flags else tools.run(flow, store)
+
+                    DRIVE = build('drive', 'v3', http=creds.authorize(Http()))
+
+                    FILES = (
+                        ('video.mp4'), #업로드 할 파일 이름
+                    )
+
+                    folder_id = '154vOPOdG3X2ibSmo6KlrhekP0Qe4QIz_' # 업로드 될 구글 드라이브의 위치 
+
+                    for file_title in FILES :
+                        file_name = file_title
+                        metadata = {'name': 'video1.mp4', # 업로드 될 파일 이름
+                                    'parents' : [folder_id],
+                                    'mimeType': None
+                                    }
+
                     cam_on = False
-                    s += "package disappeared! "
+                    s += "package disappeared! "    # add to string
                 
             # Write results
             for *xyxy, conf, cls in reversed(det):
